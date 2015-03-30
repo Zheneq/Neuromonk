dd817ac423e8b02a222c5898cb8526c08995b8e7
Dandelion <bav.dandellion@gmail.com>
2015-03-30 19:31:14 +0300

    Create grid of hexes. Radius is arbitrary. Original field radius is 2, extended field has radius 3. Set radius 2 as default.

---
+++ src/grid.py
@@ -0,0 +1,51 @@
+__author__ = 'Dandelion'
+
+
+class Cell(object):
+    def __init__(self):
+        self.tile = None
+        self.turn = 0
+        self.neighbours = [None for ind in xrange(6)]
+
+    def next(self, ind):
+        return self.neighbours[(ind + 1) % 6]
+
+    def prev(self, ind):
+        return self.neighbours[(ind - 1) % 6]
+
+    def rightind(self, ind):
+        return (ind - 1) % 6
+
+    def leftind(self, ind):
+        return (ind + 1) % 6
+
+    def oppose(self, ind):
+        return (ind + 3) % 6
+
+
+class Grid(object):
+    def __init__(self, radius=2):
+        self.cells = [Cell()]
+        for rad in xrange(radius):
+            buffer = []
+            for cell in self.cells:
+                for ind in xrange(len(cell.neighbours)):
+                    if cell.neighbours[ind] is None:
+                        buffer.append(Cell())
+                        last_cell = buffer[len(buffer) - 1]
+                        if cell.next(ind) is not None:
+                            self.link(cell.next(ind), last_cell, cell.rightind(ind))
+                        if cell.prev(ind) is not None:
+                            self.link(cell.prev(ind), last_cell, cell.leftind(ind))
+                        self.link(cell, last_cell, ind)
+            self.cells.extend(buffer)
+
+    def link(self, cell1, cell2, direction):
+        cell1.neighbours[direction] = cell2
+        cell2.neighbours[cell1.oppose(direction)] = cell1
+
+
+if __name__ == "__main__":
+    grid = Grid(2)
+    print "Yay!"
+
