dcf81543a19440816640395c844052e4f6926c91
Dandelion <bav.dandellion@gmail.com>
2015-03-31 18:11:33 +0300

    Add multiple attack.

--- src/game.py
+++ src/game.py
@@ -22,13 +22,18 @@ def compute_initiative(cell):
 
 
 def battle(grid):
+    # prepare to battle
     # find max initiative
     max_initiative = 0
     for cell in grid.cells:
         if cell.tile is not None and type(cell.tile) == Unit:
+            # reset initiative
+            for initiative_ind in xrange(len(cell.tile.initiative)):
+                cell.tile.initiative[initiative_ind][1] = True
+            # find max initiative
             initiative_modificator = compute_initiative(cell)
-            if cell.tile.initiative + initiative_modificator > max_initiative:
-                max_initiative = cell.tile.initiative + initiative_modificator
+            if cell.tile.initiative[0][0] + initiative_modificator > max_initiative:
+                max_initiative = cell.tile.initiative[0][0] + initiative_modificator
     # battle
     for phase in range(max_initiative + 1)[::-1]:
         # phase of giving damage
@@ -43,27 +48,33 @@ def give_damage_phase(grid, phase):
             # TODO get all buffs and debuffs
             # gathering all buffs of initiative
             initiative_modificator = compute_initiative(cell)
-            if phase == cell.tile.initiative + initiative_modificator:
-                for ind in xrange(len(cell.neighbours)):
-                    damage = cell.tile.damage(ind - cell.turn)
-                    if damage['melee'] > 0:
-                        if cell.neighbours[ind] is not None and cell.neighbours[ind].tile is not None:
-                            if cell.neighbours[ind].tile.army_id != cell.tile.army_id:
-                                damage_to_unit = {'value': damage['melee'], 'type': 'melee', 'instigator': cell.tile}
-                                cell.neighbours[ind].tile.taken_damage.append(damage_to_unit)
-                    if damage['range'] > 0:
-                        neighbour = cell.neighbours[ind]
-                        while neighbour is not None:
-                            if neighbour.tile is not None and neighbour.tile.army_id != cell.tile.army_id:
-                                range_damage = damage['range'] - neighbour.tile.get_armor(ind - neighbour.turn)
-                                if range_damage > 0:
-                                    damage_to_unit = {'value': range_damage,
-                                                      'type': 'range',
-                                                      'instigator': cell.tile}
-                                    neighbour.tile.taken_damage.append(damage_to_unit)
-                                if not cell.tile.row_attack:
-                                    break
-                            neighbour = neighbour.neighbours[ind]
+            for initiative_ind in xrange(len(cell.tile.initiative)):
+                if cell.tile.initiative[initiative_ind][1] and \
+                                phase == cell.tile.initiative[initiative_ind][0] + initiative_modificator:
+                    # give damage
+                    for ind in xrange(len(cell.neighbours)):
+                        damage = cell.tile.damage(ind - cell.turn)
+                        if damage['melee'] > 0:
+                            if cell.neighbours[ind] is not None and cell.neighbours[ind].tile is not None:
+                                if cell.neighbours[ind].tile.army_id != cell.tile.army_id:
+                                    damage_to_unit = {'value': damage['melee'], 'type': 'melee', 'instigator': cell.tile}
+                                    cell.neighbours[ind].tile.taken_damage.append(damage_to_unit)
+                        if damage['range'] > 0:
+                            neighbour = cell.neighbours[ind]
+                            while neighbour is not None:
+                                if neighbour.tile is not None and neighbour.tile.army_id != cell.tile.army_id:
+                                    range_damage = damage['range'] - neighbour.tile.get_armor(ind - neighbour.turn)
+                                    if range_damage > 0:
+                                        damage_to_unit = {'value': range_damage,
+                                                          'type': 'range',
+                                                          'instigator': cell.tile}
+                                        neighbour.tile.taken_damage.append(damage_to_unit)
+                                    if not cell.tile.row_attack:
+                                        break
+                                neighbour = neighbour.neighbours[ind]
+                    # disable attack in this phase
+                    cell.tile.initiative[initiative_ind][1] = False
+                    break
 
 
 def take_damage_phase(grid):
@@ -82,17 +93,17 @@ def take_damage_phase(grid):
 if __name__ == "__main__":
     playground = Grid(1)
 
-    outpost_kicker = Unit(0, 1, (1,0,0,0,0,0), (0,0,0,0,0,0), (0,0,0,0,0,0), 2)
+    outpost_kicker = Unit(0, 1, (1,0,0,0,0,0), (0,0,0,0,0,0), (0,0,0,0,0,0), [[2, True], [1, True]])
     outpost_scout = Module(0, 1, {'initiative': [1,1,0,0,0,1]}, {})
-    outpost_saboteur = Module(0, 1, {}, {'initiative': [1,1,0,0,0,1]})
-    moloch_greaver = Unit(1, 1, (0,0,0,0,0,0), (0,0,0,0,0,0), (1,1,0,0,0,1), 3)
+    moloch_fat = Unit(1, 3, (0,0,0,0,0,0), (0,0,0,0,0,0), (0,0,0,0,0,0), [[0, True]])
+    moloch_greaver = Unit(1, 1, (1,0,0,0,0,0), (0,0,0,0,0,0), (0,0,0,0,0,0), [[3, True]])
 
     playground.cells[0].tile = outpost_kicker
     playground.cells[0].turn = 1
-    playground.cells[1].tile = outpost_saboteur
-    playground.cells[1].turn = 2
-    playground.cells[2].tile = moloch_greaver
-    playground.cells[2].turn = 4
+    playground.cells[2].tile = moloch_fat
+    playground.cells[2].turn = 0
+    playground.cells[3].tile = moloch_greaver
+    playground.cells[3].turn = 4
     playground.cells[4].tile = outpost_scout
     playground.cells[4].turn = 0
 
